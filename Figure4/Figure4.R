78b943be3b3c813ff32142dc242dd20c357570b3
nightkid03 <73756135+nightkid03@users.noreply.github.com>
2026-03-06 10:33:36 -0600

    Update Figure4.R

--- Figure4/Figure4.R
+++ Figure4/Figure4.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 #Figure4B
 data=readRDS("Fig4B.rds")
 ggplot(data,aes(x=specificity,y=sensitivity))+
@@ -6,14 +8,13 @@ ggplot(data,aes(x=specificity,y=sensitivity))+
   labs(x="1-Specificity",y="Sensitivity")+
   theme_bw()+
   theme(text=element_text(size = 40))+
-  annotate("text",size=10,x=0.8,y=0.25,label=paste("AUC = ",round(g$auc,2),
-                                                   "\n95% CI: (",round(ci95$ci[1],2),"-",round(ci95$ci[3],2),")",
-                                                   sep = ""))
+  annotate("text",size=10,x=0.8,y=0.25,label="AUC = 0.68\n95% CI: (0.4-0.96)")
+
 #Figure4C
 yintercept=0.43
 aucvalule=0.76
 data=readRDS("Fig4C.rds")
-ggplot(AUPRC_draw,aes(x=rev(Recall),y=rev(Precision)))+
+ggplot(data,aes(x=rev(Recall),y=rev(Precision)))+
   geom_line(size=3)+
   #geom_segment(x=0,y=0,xend=1,yend=1,size=2,color="grey",linetype=2)+
   geom_hline(yintercept=yintercept,color="grey",linetype=2,size=2)+
@@ -29,9 +30,8 @@ ggplot(data,aes(x=specificity,y=sensitivity))+
   labs(x="1-Specificity",y="Sensitivity")+
   theme_bw()+
   theme(text=element_text(size = 40))+
-  annotate("text",size=10,x=0.8,y=0.25,label=paste("AUC = ",round(g$auc,2),
-                                                   "\n95% CI: (",round(ci95$ci[1],2),"-",round(ci95$ci[3],2),")",
-                                                   sep = ""))
+  annotate("text",size=10,x=0.8,y=0.25,label="AUC = 0.83\n95% CI: (0.68-0.98)")
+
 #Figure4E
 yintercept=0.34
 aucvalule=0.67
@@ -52,9 +52,8 @@ ggplot(data,aes(x=specificity,y=sensitivity))+
   labs(x="1-Specificity",y="Sensitivity")+
   theme_bw()+
   theme(text=element_text(size = 40))+
-  annotate("text",size=10,x=0.8,y=0.25,label=paste("AUC = ",round(g$auc,2),
-                                                   "\n95% CI: (",round(ci95$ci[1],2),"-",round(ci95$ci[3],2),")",
-                                                   sep = ""))
+  annotate("text",size=10,x=0.8,y=0.25,label="AUC = 0.75\n95% CI: (0.52-0.99)")
+
 #Figure4G
 yintercept=0.11
 aucvalule=0.42
